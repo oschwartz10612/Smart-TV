2a259bbf74292b9912458fccafa5df30e137d9e2
Owen Schwartz <oschwartz10612@gmail.com>
2020-03-27 17:17:42 -0400

    first commit

---
+++ arduino/src/main.cpp
@@ -0,0 +1,346 @@
+#include <arduino.h>
+
+int IRledPin = 13; // LED connected to digital pin 13
+int input = 2;
+int state;
+boolean onOff = false; // true = on
+
+// This procedure sends a 38KHz pulse to the IRledPin
+// for a certain # of microseconds. We'll use this whenever we need to send codes
+void pulseIR(long microsecs)
+{
+    // we'll count down from the number of microseconds we are told to wait
+
+    cli(); // this turns off any background interrupts
+
+    while (microsecs > 0)
+    {
+        // 38 kHz is about 13 microseconds high and 13 microseconds low
+        digitalWrite(IRledPin, HIGH); // this takes about 3 microseconds to happen
+        delayMicroseconds(10);        // hang out for 10 microseconds
+        digitalWrite(IRledPin, LOW);  // this also takes about 3 microseconds
+        delayMicroseconds(10);        // hang out for 10 microseconds
+
+        // so 26 microseconds altogether
+        microsecs -= 26;
+    }
+
+    sei(); // this turns them back on
+}
+
+void sendSoundUp()
+{
+    delayMicroseconds(4696);
+    pulseIR(4560);
+    delayMicroseconds(4680);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(500);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(1820);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(620);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(500);
+    delayMicroseconds(640);
+    pulseIR(520);
+    delayMicroseconds(1780);
+    pulseIR(520);
+    delayMicroseconds(1800);
+    pulseIR(520);
+}
+void sendSound()
+{
+    delayMicroseconds(5244);
+    pulseIR(4600);
+    delayMicroseconds(4620);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(580);
+    pulseIR(560);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(560);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(580);
+    delayMicroseconds(1740);
+    pulseIR(560);
+    delayMicroseconds(1740);
+    pulseIR(580);
+}
+void sendTV()
+{
+    delayMicroseconds(22216);
+    pulseIR(4680);
+    delayMicroseconds(4620);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(48280);
+    pulseIR(4660);
+    delayMicroseconds(4640);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(580);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(560);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(560);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1700);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+    delayMicroseconds(1720);
+    pulseIR(600);
+    delayMicroseconds(1720);
+    pulseIR(580);
+}
+
+void setup()
+{
+    // initialize the IR digital pin as an output:
+    pinMode(IRledPin, OUTPUT);
+    pinMode(input, INPUT);
+}
+
+void loop()
+{
+    state = digitalRead(input);
+    //Serial.println(state);
+    if (state == LOW && onOff == false)
+    {
+        sendTV();
+        delay(100);
+        sendSound();
+        delay(30000);
+        for (int i = 0; i < 21; i++)
+        {
+            sendSoundUp();
+            delay(500);
+        }
+        onOff = true;
+    }
+    else if (state == LOW && onOff == true)
+    {
+        sendTV();
+        delay(100);
+        sendSound();
+        delay(100);
+        sendSound();
+        onOff = false;
+    }
+}
---
+++ esp/src/main.cpp
@@ -0,0 +1,129 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Arduino.h>
+#include "keys.h"
+
+// Update these with values suitable for your network.
+
+const char *mqtt_server = "192.168.1.183";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+String lastState = "";
+
+void setup_wifi()
+{
+
+    delay(10);
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(WIFI_NAME);
+
+    WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    randomSeed(micros());
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+
+    payload[length] = '\0';
+    String state = String((char *)payload);
+
+    Serial.print(state);
+    Serial.println();
+
+    if (state == "ON" && lastState != "ON")
+    {
+        digitalWrite(2, LOW);
+        delay(500);
+        digitalWrite(2, HIGH);
+        client.publish("home-assistant/theater/tv", "ON");
+        lastState = "ON";
+    }
+    if (state == "OFF" && lastState != "OFF")
+    {
+        digitalWrite(2, LOW);
+        delay(500);
+        digitalWrite(2, HIGH);
+        delay(500);
+        digitalWrite(2, LOW);
+        delay(500);
+        digitalWrite(2, HIGH);
+        client.publish("home-assistant/theater/tv", "OFF");
+        lastState = "OFF";
+    }
+    
+}
+
+void reconnect()
+{
+    // Loop until we're reconnected
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        // Create a random client ID
+        String clientId = "ESP8266Client-";
+        clientId += String(random(0xffff), HEX);
+        // Attempt to connect
+        if (client.connect(clientId.c_str()))
+        {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            client.publish("home-assistant/theater/tv/available", "online");
+            // ... and resubscribe
+            client.subscribe("home-assistant/theater/tv/set");
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+void setup()
+{
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
+    Serial.begin(115200);
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
+}
+
+void loop()
+{
+
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+
+    unsigned long now = millis();
+    if (now - lastMsg > 60000)
+    {
+        lastMsg = now;
+        client.publish("home-assistant/theater/tv/available", "online");
+    }
+}
